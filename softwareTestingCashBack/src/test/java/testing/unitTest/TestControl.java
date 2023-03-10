e26d0dba196880acc8c7bc69eaad3a8913843e12
annaamodio <anna.amodio@studenti.unina.it>
2023-03-10 11:48:37 +0100

    Test del livello control con JMockit

--- softwareTestingCashBack/src/test/java/testing/unitTest/TestControl.java
+++ softwareTestingCashBack/src/test/java/testing/unitTest/TestControl.java
@@ -2,23 +2,20 @@ package testing.unitTest;
 
 import control.CGestioneCashback;
 import entity.ApplicazioneCashback;
-import mockit.Expectations;
-import mockit.Mocked;
-import mockit.Tested;
-import mockit.Verifications;
+import mockit.*;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestControl {
-    private static ApplicazioneCashback appCash;
+    @Mocked private ApplicazioneCashback appCash;// stattic non funziona
     @Tested static CGestioneCashback gestioneCashback;
 
     @Test
     public void test1()throws Exception {
-        appCash=ApplicazioneCashback.getInstance(); // classe statica abbiamo fatto un mocking parziale.
-        new Expectations(appCash) {{
+        new Expectations() {{
+            ApplicazioneCashback.getInstance(); result = appCash;
             appCash.richiediRimborso("ABCDEFGHI123456", "qwerty7890", 162022, withNull());result = (float) 2.0;
             appCash.richiediRimborso(withNotEqual("ABCDEFGHI123456"), anyString, anyInt, withNull());result=new IllegalArgumentException();
 
@@ -26,6 +23,7 @@ public class TestControl {
         assertEquals(gestioneCashback.gestisciRichiediRimborso("ABCDEFGHI123456", "qwerty7890", 162022), (float) 2.0);
         assertThrows(IllegalArgumentException.class,()->gestioneCashback.gestisciRichiediRimborso("ABCDEFGHI1@3456", "qwerty!890", 162022));
         new Verifications(){{
+            ApplicazioneCashback.getInstance(); times = 2;
             appCash.richiediRimborso("ABCDEFGHI123456", "qwerty7890", 162022, null);
             times = 1;
         }};
