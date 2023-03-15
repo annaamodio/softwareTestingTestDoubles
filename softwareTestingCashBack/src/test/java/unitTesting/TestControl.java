daef1640f4452c71fa2cb2eb0ac3d97083bb003c
annaamodio <anna.amodio@studenti.unina.it>
2023-03-15 16:39:49 +0100

    rimborso = 10

--- softwareTestingCashBack/src/test/java/unitTesting/TestControl.java
+++ softwareTestingCashBack/src/test/java/unitTesting/TestControl.java
@@ -15,11 +15,11 @@ public class TestControl {
     @Test
     public void test1()throws Exception {
         new Expectations() {{
-            appCash.richiediRimborso("ABCDEFGHI123456", "qwerty7890", 162022, withNull());result = (float) 2.0;
+            appCash.richiediRimborso("ABCDEFGHI123456", "qwerty7890", 162022, withNull());result = (float) 10;
             appCash.richiediRimborso(withNotEqual("ABCDEFGHI123456"), anyString, anyInt, withNull());result=new IllegalArgumentException();
 
         }};
-        assertEquals(gestioneCashback.gestisciRichiediRimborso("ABCDEFGHI123456", "qwerty7890", 162022), (float) 2.0);
+        assertEquals(gestioneCashback.gestisciRichiediRimborso("ABCDEFGHI123456", "qwerty7890", 162022), (float) 10);
         assertThrows(IllegalArgumentException.class,()->gestioneCashback.gestisciRichiediRimborso("ABCDEFGHI1@3456", "qwerty!890", 162022));
         new Verifications(){{
             ApplicazioneCashback.getInstance(); times = 2;
